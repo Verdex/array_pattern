9bc7b888b6d39208130aa373fb6816da543f7f80
David L. Inman <davidlevere@gmail.com>
2022-03-22 00:23:07 -0400

    inc prog for seq!

--- src/lib.rs
+++ src/lib.rs
@@ -13,38 +13,35 @@ pub enum MatchResult<T> {
 #[macro_export]
 macro_rules! seq {
 
+    // TODO predicates?
+    // TODO pat has given me some problems with literals
     // TODO be able to call other parsers
     // TODO reset on failure
 
-    (err, $input:ident, $n:ident <= $p:pat, $($rest:tt)*) => {
+    (err, $rp:ident, $input:ident, $n:ident <= $p:pat, $($rest:tt)*) => {
         let $n = match $input.next() {
             Some(y @ (i, $p)) => y,
             _ => { return MatchResult::Error; },
         };
-        seq!(fatal, $input, $($rest)*);
+        seq!(fatal, $rp, $input, $($rest)*);
     };
 
-    (fatal, $input:ident, $n:ident <= $p:pat, $($rest:tt)*) => {
+    (fatal, $rp:ident, $input:ident, $n:ident <= $p:pat, $($rest:tt)*) => {
         let $n = match $input.next() {
             Some(y @ (i, $p)) => y,
             _ => { return MatchResult::Fatal(0); },
         };
-        seq!(fatal, $input, $($rest)*);
+        seq!(fatal, $rp, $input, $($rest)*);
     };
 
-    ($mode:ident, $input:ident, $b:block) => {
+    ($mode:ident, $rp:ident, $input:ident, $b:block) => {
         return MatchResult::Success(0, $b);
     };
 
     ($name:ident<$o:lifetime> : $in_t:ty => $out_t:ty = $($rest:tt)*) => {
-        fn $name<$o>(input : &mut impl Iterator<Item = (usize, $in_t)>) -> MatchResult<$out_t> {
-            seq!(err, input, $($rest)*);
-        }
-    };
-
-    ($name:ident : $in_t:ty => $out_t:ty = $($rest:tt)*) => {
-        fn $name(input : &mut impl Iterator<Item = (usize, $in_t)>) -> MatchResult<$out_t> {
-            seq!(err, input, $($rest)*);
+        fn $name<$o>(input : &mut (impl Iterator<Item = (usize, $in_t)> + Clone)) -> MatchResult<$out_t> {
+            let rp = input.clone();
+            seq!(err, rp, input, $($rest)*);
         }
     };
 }
