dce01bbaab838b1f3d776491e06927f97c181499
David L. Inman <davidlevere@gmail.com>
2022-03-21 22:05:24 -0400

    array_matcher

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+
+#[macro_export]
+macro_rules! array_matcher {
+
+    ($input:ident, $n:ident <= $p:pat, $($rest:tt)*) => {
+        let $n = match $input.next() {
+            Some(y @ $p) => y,
+            _ => { return None; },
+        };
+        array_matcher!($input, $($rest)*);
+    };
+
+    ($input:ident, $b:block) => {
+        return Some($b);
+    };
+
+    ($name:ident<$o:lifetime> : $in_t:ty => $out_t:ty = $($rest:tt)*) => {
+        fn $name<$o>(input : &mut impl Iterator<Item = $in_t>) -> Option<$out_t> {
+            array_matcher!(input, $($rest)*);
+        }
+    };
+
+    ($name:ident : $in_t:ty => $out_t:ty = $($rest:tt)*) => {
+        fn $name(input : &mut impl Iterator<Item = $in_t>) -> Option<$out_t> {
+            array_matcher!(input, $($rest)*);
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
